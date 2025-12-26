fe5c4c845cab7798ef58fb9f7fadc665163a265a
Shea Levy <shea@shealevy.com>
2025-12-26 09:27:31 -0500

    capio: Add bracket

--- capio/CapIO.hs
+++ capio/CapIO.hs
@@ -1,5 +1,9 @@
+{-# LANGUAGE CPP #-}
+
 module CapIO where
 
+import Control.Exception
+import Data.Coerce
 import System.IO.Unsafe
 
 type CapIO = IO
@@ -16,3 +20,25 @@ runCapIO go = go MkIOCap
 
 runPureIO :: (PureCap -> CapIO a) -> a
 runPureIO go = unsafePerformIO (go MkPureCap)
+
+#if !MIN_VERSION_base(4,21,0)
+newtype NoBacktrace e = NoBacktrace e deriving (Show)
+instance Exception e => Exception (NoBacktrace e) where
+  fromException = coerce (fromException @e)
+  toException = coerce (toException @e)
+  backtraceDesired _ = False
+#endif
+
+bracket :: CapIO a -> (a -> Either SomeException b -> CapIO ()) -> (a -> CapIO b) -> CapIO b
+bracket acquire release go = mask $ \unmask -> do
+  a <- acquire
+  b <- catchNoPropagate (unmask $ go a) $ \e@(ExceptionWithContext _ se) -> do
+    release a (Left se)
+    rethrowIO e
+  release a (Right b)
+  pure b
+ where
+#if !MIN_VERSION_base(4,21,0)
+    rethrowIO = throwIO . NoBacktrace
+    catchNoPropagate = catch
+#endif
