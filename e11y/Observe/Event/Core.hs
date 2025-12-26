4dc112c1d18e53cdd6156d0df38a26e79686d8b7
Shea Levy <shea@shealevy.com>
2025-12-25 20:51:15 -0500

    e11y: EventBackends

--- e11y/Observe/Event/Core.hs
+++ e11y/Observe/Event/Core.hs
@@ -22,3 +22,16 @@ class Event ev where
   reference :: ev field -> EventReference ev
   finalize :: ev field -> Maybe SomeException -> CapIO ()
   addField :: ev field -> field -> CapIO ()
+
+data EventParams backend selector field = EventParams
+  { selectors :: !(EventSelectors selector field)
+  , parent :: !(Maybe (EventReference (BackendEvent backend)))
+  , causes :: ![EventReference (BackendEvent backend)]
+  , initialFields :: ![field]
+  }
+
+class (Event (BackendEvent backend)) => EventBackend backend where
+  type BackendEvent backend :: Type -> Type
+
+  newEvent :: backend selector -> EventParams backend selector field -> CapIO (BackendEvent backend field)
+  newInstantEvent :: backend selector -> EventParams backend selector field -> CapIO (EventReference (BackendEvent backend))
