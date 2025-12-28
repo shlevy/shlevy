9af25c74d123813468402103200fb2acf224970a
Shea Levy <shea@shealevy.com>
2025-12-28 10:10:50 -0500

    capio: Allocate, ConcurrentAllocate

--- capio/CapIO.hs
+++ capio/CapIO.hs
@@ -1,4 +1,7 @@
 {-# LANGUAGE CPP #-}
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module CapIO
   ( CapIO
@@ -7,18 +10,33 @@ module CapIO
   , purity
   , runCapIO
   , runPureIO
-  , bracket
-  , bracketOnError
   , onException
   , finally
+  , with
+  , Allocate (..)
+  , Restore (..)
+  , Allocated (..)
+  , ConcurrentAllocate
+  , concurrentAllocate
+  , runConcurrentAllocate
+  , Concurrently (..)
   )
 where
 
+import Control.Applicative
+import Control.Concurrent
+import Control.Concurrent.Async hiding (Concurrently)
+import Control.Concurrent.Async qualified as Async
+import Control.Concurrent.STM.TVar
 import Control.Exception hiding (bracket, bracketOnError, finally, onException)
 import Control.Exception qualified as CE
-#if !MIN_VERSION_base(4,21,0)
+import Control.Monad
+import Control.Monad.STM
 import Data.Coerce
-#endif
+import Data.Functor.Compose
+import Data.Profunctor
+import Data.Profunctor.Cayley
+import Data.Void
 import System.IO.Unsafe
 
 type CapIO = IO
@@ -53,21 +71,6 @@ catchNoPropagate :: (Exception e) => IO a -> (e -> IO a) -> IO a
 catchNoPropagate = catch
 #endif
 
-bracket :: CapIO a -> (a -> Either SomeException b -> CapIO ()) -> (a -> CapIO b) -> CapIO b
-bracket acquire release go = mask $ \unmask -> do
-  a <- acquire
-  b <- catchNoPropagate (unmask $ go a) $ \e@(ExceptionWithContext _ se) -> do
-    annotateWhileHandling se $ release a (Left se)
-    rethrowIO e
-  release a (Right b)
-  pure b
-
-bracketOnError :: CapIO a -> (a -> SomeException -> CapIO ()) -> (a -> CapIO b) -> CapIO b
-bracketOnError acquire release = bracket acquire release'
- where
-  release' _ (Right _) = pure ()
-  release' a (Left e) = release a e
-
 onException :: CapIO a -> (SomeException -> CapIO ()) -> CapIO a
 onException go cleanup = catchNoPropagate go $ \e@(ExceptionWithContext _ se) -> do
   annotateWhileHandling se $ cleanup se
@@ -75,3 +78,120 @@ onException go cleanup = catchNoPropagate go $ \e@(ExceptionWithContext _ se) ->
 
 finally :: CapIO a -> CapIO () -> CapIO a
 finally = CE.finally
+
+with :: Allocate a res -> (res -> CapIO a) -> CapIO a
+with alloc body = mask $ \restore' -> do
+  let restore = Restore restore'
+  MkAllocated{..} <- coerce alloc restore
+  x <- restore' (body resource) `onException` (release restore . Left)
+  release restore $ Right x
+  pure x
+
+newtype Restore = Restore (forall x. CapIO x -> CapIO x)
+
+newtype Allocate b a = MkAllocate (Restore -> CapIO (Allocated b a))
+  deriving (Functor) via (Compose ((->) Restore) (Compose CapIO (Allocated b)))
+  deriving (Profunctor) via (Cayley (Compose ((->) Restore) CapIO) Allocated)
+
+instance Applicative (Allocate b) where
+  pure :: forall a. a -> Allocate b a
+  pure = coerce (pure @(Compose ((->) Restore) (Compose CapIO (Allocated b))) @a)
+  (<*>) = ap
+
+instance Monad (Allocate b) where
+  return = pure
+  allocX >>= (f :: a -> Allocate b c) = coerce @(Restore -> CapIO (Allocated b c)) $ \restore -> do
+    allocatedX@(MkAllocated (x :: a) releaseX) <- coerce allocX restore
+    allocatedY <- coerce f x restore `onException` (releaseX restore . Left)
+    pure $ allocatedX *> allocatedY
+
+data Allocated b a = MkAllocated
+  { resource :: a
+  , release :: Restore -> Either SomeException b -> CapIO ()
+  }
+  deriving stock (Functor)
+
+instance Profunctor Allocated where
+  dimap f g MkAllocated{..} =
+    MkAllocated
+      { resource = g resource
+      , release = \restore -> release restore . fmap f
+      }
+
+instance Applicative (Allocated b) where
+  pure x = MkAllocated x (\_ _ -> pure ())
+  (<*>) = ap
+
+instance Monad (Allocated b) where
+  return = pure
+  (MkAllocated{..}) >>= f =
+    MkAllocated
+      { resource = fResource
+      , release = \restore b -> fRelease restore b `finally` release restore b
+      }
+   where
+    MkAllocated{resource = fResource, release = fRelease} = f resource
+
+newtype ConcurrentAllocate b a = MkConcurrentAllocate (Allocate Void (STM (Either SomeException (Allocated b a)))) deriving (Functor) via Compose (Allocate Void) (Compose STM (Compose (Either SomeException) (Allocated b)))
+
+concurrentAllocate :: forall b a. Allocate b a -> ConcurrentAllocate b a
+concurrentAllocate alloc = coerce @(Restore -> CapIO (Allocated Void (STM (Either SomeException (Allocated b a))))) $ \_ -> do
+  needsRelease <- newTVarIO Nothing
+  allocationDone <- newTVarIO False
+  allocAsync <- asyncWithUnmask $ \restore -> do
+    allocated <- coerce alloc (Restore restore)
+
+    nr <- atomically $ do
+      writeTVar allocationDone True
+      readTVar needsRelease
+    case nr of
+      Nothing -> pure ()
+      Just b -> do
+        release allocated (Restore restore) (absurd <$> b)
+        allowInterrupt
+
+    pure allocated
+
+  pure $
+    MkAllocated
+      { resource = waitCatchSTM allocAsync
+      , release = \restore b -> do
+          ad <- atomically $ do
+            writeTVar needsRelease (Just b)
+            readTVar allocationDone
+          case ad of
+            True ->
+              uninterruptibleMask_ (waitCatch allocAsync) >>= \case
+                Left _ -> pure ()
+                Right allocated -> release allocated restore (absurd <$> b)
+            False -> void $ forkIO $ cancel allocAsync
+      }
+
+instance Applicative (ConcurrentAllocate b) where
+  pure :: forall a. a -> ConcurrentAllocate b a
+  pure = coerce (pure @(Compose (Allocate Void) (Compose STM (Compose (Either SomeException) (Allocated b)))) @a)
+  liftA2 (f :: a -> b' -> c) ca1 ca2 = coerce @(Allocate Void (STM (Either SomeException (Allocated b c)))) $ do
+    s1 <- coerce ca1
+    s2 <- coerce ca2
+    pure $
+      (fmap Left <$> s1) <|> (fmap Right <$> s2) >>= \case
+        Left e -> pure $ Left e
+        Right (Left x) -> fmap (liftA2 f x) <$> s2
+        Right (Right y) -> do
+          x <- s1
+          pure $ liftA2 (liftA2 f) x (pure y)
+
+runConcurrentAllocate :: forall b a. ConcurrentAllocate b a -> Allocate b a
+runConcurrentAllocate ca = coerce @(Restore -> CapIO (Allocated b a)) $ \restore -> do
+  MkAllocated{..} <- coerce ca restore
+  atomically resource `onException` (release restore . Left @_ @Void) >>= \case
+    Left e -> do
+      annotateWhileHandling e $ release restore (Left e)
+      throwIO e
+    Right alloced -> pure alloced
+
+newtype Concurrently a = Concurrently {runConcurrently :: CapIO a}
+  deriving (Functor) via Async.Concurrently
+  deriving (Applicative) via Async.Concurrently
+  deriving (Semigroup) via (Async.Concurrently a)
+  deriving (Monoid) via (Async.Concurrently a)
